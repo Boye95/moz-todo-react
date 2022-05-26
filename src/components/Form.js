7001451cad01a77254d98eecd4a06394b90824b3
Boye <samuel.boye95@yahoo.com>
2022-05-26 23:05:11 +0100

    add test function

--- src/components/Form.js
+++ src/components/Form.js
@@ -1,8 +1,13 @@
 import React from "react";
 
 export default function Form(props) {
+  function handleSubmit(e) {
+    e.preventDefault();
+    alert("Hello, world!");
+  }
+
   return (
-    <form>
+    <form onSubmit={handleSubmit}>
       <h2 className="label-wrapper">
         <label htmlFor="new-todo-input" className="label__lg">
           What needs to be done?
